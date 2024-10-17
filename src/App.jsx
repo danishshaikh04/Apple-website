2e2a6bd814a4e77c17c94f5585e69854657670c6
danishshaikh04 <danishshaikh0595@gmail.com>
2024-10-17 16:35:07 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Navbar from "./components/Navbar"
+import Model from "./components/Model"
+
 
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
