0a9e7b379d59d064532ba7a24fcc4cffb8c8a30b
danishshaikh04 <danishshaikh0595@gmail.com>
2024-10-18 16:54:56 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//...
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://d3e2ecb5e588d1e40e664e133bffe2c7@o4508142342569984.ingest.de.sentry.io/4508142345060432",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0, 
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1, 
-  replaysOnErrorSampleRate: 1.0, 
-});
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
